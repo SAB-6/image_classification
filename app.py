2aa615b6044067f8d97b76c128a7e2cc33bf112e
SAB-6 <shereef.bankole@yahoo.com>
2021-09-20 10:57:31 +0100

    small changes

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, request, jsonify, render_template
 import os
+
+from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS, cross_origin
-from com_in_ineuron_ai_utils.utils import decodeImage
+
+from image_utils.utils import decodeImage
 from predict import dogcat
 
-os.putenv('LANG', 'en_US.UTF-8')
-os.putenv('LC_ALL', 'en_US.UTF-8')
+os.putenv("LANG", "en_US.UTF-8")
+os.putenv("LC_ALL", "en_US.UTF-8")
 
 app = Flask(__name__)
 CORS(app)
@@ -18,16 +20,16 @@ class ClientApp:
         self.classifier = dogcat(self.filename)
 
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=["GET"])
 @cross_origin()
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route("/predict", methods=['POST'])
+@app.route("/predict", methods=["POST"])
 @cross_origin()
 def predictRoute():
-    image = request.json['image']
+    image = request.json["image"]
     decodeImage(image, clApp.filename)
     result = clApp.classifier.predictiondogcat()
     return jsonify(result)
